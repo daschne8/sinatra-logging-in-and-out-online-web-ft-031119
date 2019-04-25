050b7d0c929fe9ebb9628c4943196e9ff789d550
Daniel Schneider <daschne8@gmail.com>
2019-04-25 17:13:11 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,11 +1,11 @@
 class Helpers
 
   def current_user(session)
-    User.find_by(id: session[:id])
+    User.find_by(id: session[:user_id])
   end
 
   def is_logged_in?(session)
-    !!current_user(session)
+    !!session[:user_id]
   end
   
 end
